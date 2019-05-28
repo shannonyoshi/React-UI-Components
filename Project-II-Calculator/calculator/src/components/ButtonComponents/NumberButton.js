da189fff7d36717d4916a591f36a6d3a7291cf2e
Shannon <shannon.yoshikawa@gmail.com>
2019-05-28 14:08:45 -0700

    working on styling for calc

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButtons = (props)=>{
+    return <button className ={props.btnType}>{props.text}</button>
+  }
+
+export default NumberButtons;
